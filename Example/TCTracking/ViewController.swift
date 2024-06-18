59185ea56f321b0a6cc8a6fc5be09ff2d0266d0c
PhuTruong <truongthienphu@vietbando.vn>
2024-06-18 14:06:31 +0700

    update example user, tenant

--- Example/TCTracking/ViewController.swift
+++ Example/TCTracking/ViewController.swift
@@ -11,10 +11,9 @@ import TCTracking
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let user = "thanh13"
-        
-        let token = "bearer f0ue9Ce6swJXku2IH8h_....."
-        let refreshToken = "rvFTI9O9oXggF6FeqvIxbWaeno57aax....."
+        let user = "sales_testing_2"
+        let token = "bearer KZXSGea-e8LQhZS-Mf-MMk6eLaoFw1eS_aBttGsnG-_PZ3vBHcxgXxS1z-mQDcsmxnlCV-Y7As6Ui8Gyw65f6vQBOmtFF0DhnwWX5X3BKBt6sGgtuWuQHF4VTYMo-HTHdZ9WatXghvddWTsJcxjLIcVKmzFYYmXblIo3gUS2Cu2Kp8wECsYXpS-xqk9RLrugjExnQjI7Xbv7jhi3hdEftY9O2xIN4XFenSJ0-SqqqzlWw6W8tVomKbKQG1PLQ0DY8DfFAmwEkYhEuiyznZ2jhYOBMAKBzVFgDfPH6O98mnjkS6tW_viSYP4SzOxWQpwucr9SSlDBTW7EcZYSYsPcjjMTR_mMqBV8psZXuRIPFL2zsIqXyIMPjMnQoIIycpPEL5k3iKQuG8hlGyrjDz5EwZEpXVvzSBAkaVijbMMrM7EgCU2qwfATs5kITWe2aUMi1FoJTJF-mFBR5XqynnrcqdrD94o"
+        let refreshToken = "9s5VvQRZPMwvT_Zfs_eTzsvmcWWxs7gwq5dZ2gOL3oyNTqIw9XeB3mAeY5ogxfJiHRGsQFC7YaPrtMrfnP7PyR9qUG-JLxfiLo0w9eZkZAQ1h2OYTnIGp1Ksd9dD4pI1mAL_zE9wnukZVam3edsEESKPxwNMe1FZybzI31d0FS78P9LrJzHZbhEn2MLosn5hYSU-h7zn5o-HmmmIu5c7M2l4vPX0O04F3iQO9zwz0WsrLPumu5FWt6KYRofcGmKh17CuuSSAIAQEwl1icHTMeMGNcbWZ7mDYAsGQpCFghglJDbC_5-U35qtYjL0kCsKKHcEUI0vvuQ6sZVG96koUIIZhvTHMZ-VQzmGO5xq8AnZ-gJH_MXgVW6ZHQzYjCQKN-UEQOBZiG5BwPUUMORy9kaZt4BTwSp9CTCixtf4XU9jBBoPu69xNaZIWAYxbqI7Yl5jt9f8nJQijYWKKE3dHPBalF0o"
 //        let expiresIn = expires_in + Date().timeIntervalSince1970
         let expiresIn = 0
 //    ApiUrl: HOST + "/api/app-base/vdms-tracking/push" // https://testing.skedulomatic.com/api/app-base/vdms-tracking/push
@@ -26,7 +25,7 @@ class ViewController: UIViewController {
 //        --data-urlencode 'username=<username>' \
 //        --data-urlencode 'password=<password>'
 
-        SVTrackingManager.shareInstance.configData(driverName: user, accessToken: token, trackingURL: "https://testing.skedulomatic.com/api/app-base/vdms-tracking/push", backendURL: "https://testing.skedulomatic.com", apiVersion: "2.7.2", jobStatus: 2)
+        SVTrackingManager.shareInstance.config(driverName: user, accessToken: token, trackingURL: "https://sales.grow-matic.com/api/app-base/vdms-tracking/push", backendURL: "https://testing.skedulomatic.com", jobStatus: 2)
         SVTrackingManager.shareInstance.setTrackingFrequency(miliseconds: 10000)
         SVTrackingManager.shareInstance.setAuthenInfo(url:"https://accounts.skedulomatic.com/oauth/token", refreshToken: refreshToken, expiresIn:String(expiresIn))
         SVTrackingManager.shareInstance.setUseMotionSensor(enable: true)
